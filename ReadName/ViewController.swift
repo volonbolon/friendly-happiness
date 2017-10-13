bafcd2d0fbf2ce76b9ee1911bbbf52f163894383
Ariel Rodriguez <ariel@volonbolon.me>
2017-10-13 20:31:08 -0300

    Initial Commit

---
+++ ReadName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadName
+//
+//  Created by Ariel Rodriguez on 10/13/17.
+//  Copyright © 2017 Volonbolon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
