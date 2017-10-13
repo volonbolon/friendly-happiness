a442bcf2942e22363115eb25b1a001c4f2f37fec
Ariel Rodriguez <ariel@volonbolon.me>
2017-10-13 20:45:07 -0300

    Add Xcode Project

--- ReadName/ViewController.swift
+++ ReadName/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let localizedString = NSLocalizedString("Hello, %@!", comment: "Greetings")
+        let formattedString = String.localizedStringWithFormat(localizedString, textField.text!)
+        self.label.text = formattedString
+        return true
+    }
 }
 
